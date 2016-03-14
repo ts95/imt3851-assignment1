7e55572d833f79d9397ee869b63dfb3d4f2f6e0b
Toni S <toni.sucic@icloud.com>
2016-03-14 17:55:44 +0100

    Add comments to \Store\Collection.php & \Store\Store.php

--- store/Collection.php
+++ store/Collection.php
@@ -3,23 +3,53 @@ namespace Store;
 
 require_once __DIR__ . '/../misc/functions.php';
 
+/**
+ * A collection is a class that makes it easy to read and write
+ * from CSV files.
+ */
 class Collection {
 
+    /**
+     * @type string
+     */
     const CSV_SEPARATOR = ',';
 
+    /**
+     * @type \Store\Store
+     */
     private $store;
+    /**
+     * Name of the store.
+     * @type string
+     */
     private $name;
+    /**
+     * A list of the columns at the top of the CSV file.
+     * @type array
+     */
     private $metaRow;
+    /**
+     * A list of rows in the CSV file.
+     * @type array (2 dimensions)
+     */
     private $operationalRows;
 
+    /**
+     * Collection constructor.
+     * @param \Store\Store $store
+     * @param string $name
+     */
     public function __construct($store, $name) {
         $this->store = $store;
         $this->name = $name;
 
         if (!file_exists($this->getFilePath())) {
+            // If the file doesn't exist start off with empty properties.
             $this->metaRow = [];
             $this->operationalRows = [];
         } else {
+            // If the file does exist, read the contents of it into the properties.
+
             $f = fopen($this->getFilePath(), 'r');
 
             $lines = [];
@@ -56,14 +86,26 @@ class Collection {
         }
     }
 
+    /**
+     * Sets the columns at the top of the CSV file.
+     * @param array $attributes
+     */
     public function setAttributes($attributes) {
         $this->metaRow = $attributes;
     }
 
+    /**
+     * Gets the columns at the top of the CSV file.
+     * @return array
+     */
     public function getAttributes() {
         return $this->metaRow;
     }
 
+    /**
+     * Appends a row of data to the end of the CSV file.
+     * @param array $row
+     */
     public function addRow($row) {
         if (!is_array($row))
             throw InvalidArgumentException('$row must be an array');
@@ -73,23 +115,38 @@ class Collection {
         $this->operationalRows[] = $row;
     }
 
+    /**
+     * Searches for rows that match a given query.
+     * @param function $cb Callback. A row is passed to it as its first parameter.
+     * @return array
+     */
     public function searchRows($cb) {
         return filter($this->operationalRows, function($operationalRow) use($cb) {
             return $cb($operationalRow);
         });
     }
 
+    /**
+     * Same as #searchRows but returns one result instead of a list of results.
+     * If no result is found, null is returned.
+     * @param function $cb Callback. A row is passed to it as its first parameter.
+     * @return array
+     */
     public function searchRow($cb) {
         $rows = $this->searchRows($cb);
 
         if (count($rows) == 0) {
-            error_log('Collection::searchRow() found no matching rows.');
             return null;
         }
 
         return $rows[0];
     }
 
+    /**
+     * Updates rows in the store.
+     * @param array $values Values that overwrite the current values in the collection.
+     * @param function $cb Callback. A row is passed to it as its first parameter.
+     */
     public function updateRows($values, $cb) {
         $this->operationalRows = map($this->operationalRows, function($operationalRow) use($values, $cb) {
             if ($cb($operationalRow)) {
@@ -103,16 +160,29 @@ class Collection {
         });
     }
 
+    /**
+     * Removes rows in the store.
+     * @param array $values Values that overwrite the current values in the collection.
+     * @param function $cb Callback. A row is passed to it as its first parameter.
+     */
     public function removeRows($cb) {
         $this->operationalRows = filter($this->operationalRows, function($operationalRow) use($cb) {
             return !$cb($operationalRow);
         });
     }
 
+    /**
+     * Gets all the operational data rows in the store. Use #searchRows() to get
+     * more specific results.
+     * @return array
+     */
     public function getRows() {
         return $this->operationalRows;
     }
 
+    /**
+     * Writes changes to the CSV file.
+     */
     public function save() {
         $f = fopen($this->getFilePath(), 'w');
 
@@ -135,6 +205,10 @@ class Collection {
         fclose($f);
     }
 
+    /**
+     * Gets the path to the CSV file.
+     * @return string
+     */
     public function getFilePath() {
         return $this->store->getFolderPath() . "/$this->name.csv";
     }
--- store/Store.php
+++ store/Store.php
@@ -4,28 +4,62 @@ namespace Store;
 require_once __DIR__ . '/../misc/Helper.php';
 require_once __DIR__ . '/Collection.php';
 
+/**
+ * A store represents the folder in which the CSV files
+ * are stored. The CSV files are represented by the
+ * collection class.
+ */
 class Store {
 
+    /**
+     * A path to the folder.
+     * @type string
+     */
     private $folderPath;
 
+    /**
+     * Store constructor
+     * @param string $folderPath
+     */
     public function __construct($folderPath) {
         $this->folderPath = $folderPath;
     }
 
+    /**
+     * @return string
+     */
     public function getFolderPath() {
         return $this->folderPath;
     }
 
+    /**
+     * Checks if a given collection (CSV file) exists.
+     * @param string $name
+     * @return bool
+     */
     public function collectionExists($name) {
         return file_exists("$this->folderPath/$name.csv");
     }
 
+    /**
+     * Creates a collection.
+     * NB: The collection is not saved. That must be done manually by calling
+     * #save() on the returned object.
+     * @param string $name
+     * @param array $attributes
+     * @return \Store\Collection
+     */
     public function makeCollection($name, $attributes) {
         $collection = $this->getCollection($name);
         $collection->setAttributes($attributes);
         return $collection;
     }
 
+    /**
+     * Gets a collection.
+     * @param string $name
+     * @return \Store\Collection
+     */
     public function getCollection($name) {
         if (!file_exists($this->folderPath))
             mkdir($this->folderPath);
